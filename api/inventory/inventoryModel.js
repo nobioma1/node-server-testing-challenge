bbdf751017beec1eea3cbf3acaddebb1f86a17e4
Noble Obioma <nobioma1@gmail.com>
2019-07-25 15:10:33 +0100

    Create models to perfor CRUD operations

--- api/inventory/inventoryModel.js
+++ api/inventory/inventoryModel.js
@@ -1,9 +1,29 @@
-const db = require('../../data/dbConfig')
+const db = require('../../data/dbConfig');
 
 module.exports = {
-  getAll
+  getAll,
+  insert,
+  findById,
+  remove,
 };
+
+function findById(id) {
+  return db('products')
+    .where({ id })
+    .first();
+}
 
 function getAll() {
   return db('products');
+}
+
+async function insert(product) {
+  const [id] = await db('products').insert(product);
+  return await findById(id);
+}
+
+function remove(id) {
+  return db('products')
+    .where({ id })
+    .del();
 }
